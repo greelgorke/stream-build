8fc4f8fb07733cad7a9cff3f946c7a8bc5008e2d
greelgorke <greelgorke@gmail.com>
2013-05-02 12:58:34 +0200

    testing travis

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var stream = require('stream')
 module.exports = exports = stream
 
+
 exports.readable  = require('./builder/readable')(stream)
 exports.writable  = require('./builder/writable')(stream)
 exports.duplex    = require('./builder/duplex')(stream)
